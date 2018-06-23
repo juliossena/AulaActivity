b5c259d68b4f7c2c0bbd5398c418d3862b5d0c55
juliossena <juliosouzasena@gmail.com>
2018-06-23 17:49:03 -0300

    final

--- app/src/main/java/aulaactivity/cursoandroid/dell/aulaactivity/MainActivity.java
+++ app/src/main/java/aulaactivity/cursoandroid/dell/aulaactivity/MainActivity.java
@@ -1,13 +1,60 @@
 package aulaactivity.cursoandroid.dell.aulaactivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private Button botaoActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoActivity = (Button) findViewById(R.id.botao_id);
+
+        botaoActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, SegundaActivity.class));
+                Toast.makeText(getApplicationContext(), "Clicou", Toast.LENGTH_LONG);
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "Start", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "Resumo", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "Pausa", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "Stop", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(getApplicationContext(), "Destruir", Toast.LENGTH_LONG);
     }
 }
---
+++ app/src/main/java/aulaactivity/cursoandroid/dell/aulaactivity/SegundaActivity.java
@@ -0,0 +1,14 @@
+package aulaactivity.cursoandroid.dell.aulaactivity;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segunda);
+    }
+}
